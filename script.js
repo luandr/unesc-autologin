31294331945eaf37be5f8990ebb1f2114274f5e9
Luan Rocha <luandr@users.noreply.github.com>
2017-04-12 10:46:09 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Unesc autologin
+// @version      1
+// @description  autologin AVA Unesc
+// @author       You
+// @match        https://ead.unesc.net/ava/entrar.php?areaEnsino=20171
+// @require      http://code.jquery.com/jquery-1.12.4.min.js
+// ==/UserScript==
+
+(function() {
+    $('#login').val('HERE YOU PUT YOUR CODE');
+    $('#password').val('HERE YOU PUT YOUR PASSWORD');
+    $('.btn').click();
+})();
