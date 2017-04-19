3514139310d48b0a477781b22846c738b1d61500
Luan Rocha <luandr@users.noreply.github.com>
2017-04-19 15:42:17 -0300

    Update AVA_script.js

--- scripts/AVA_script.js
+++ scripts/AVA_script.js
@@ -2,7 +2,7 @@
 // @name         Unesc autologin
 // @version      1
 // @description  autologin AVA Unesc
-// @author       You
+// @author       Luan
 // @match        https://ead.unesc.net/ava/entrar.php?areaEnsino=20171
 // @require      http://code.jquery.com/jquery-1.12.4.min.js
 // ==/UserScript==
